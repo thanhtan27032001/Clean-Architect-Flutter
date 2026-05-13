5f83dc9bb27d81f3e871ee3720d55722c27a1802
ThanhTan <thanhtan27032001@gmail.com>
2026-05-13 10:39:40 +0700

    Setup theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architect/presentation/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -16,6 +17,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: getAppTheme(),
+    );
   }
 }
--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -7,6 +7,12 @@ class ColorManager {
   static Color lightGrey = HexColor.fromHex("#9E9E9E");
   static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
   static const Color text = Colors.black;
+
+  static Color darkPrimary = HexColor.fromHex("#d17d11");
+  static Color grey1 = HexColor.fromHex("#707070");
+  static Color grey2 = HexColor.fromHex("#797979");
+  static Color white = HexColor.fromHex("#FFFFFF");
+  static Color error = HexColor.fromHex("#e61f34"); // red color
 }
 
 extension HexColor on Color {
